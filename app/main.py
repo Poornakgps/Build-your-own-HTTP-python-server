84cc72adbb3ec18c19cc36301faf3596f33637ef
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2024-05-21 20:41:16 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,30 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept()  # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")  # wait for client
+    
+    conn, addr = server_socket.accept()  # wait for client
+    
+    data = conn.recv(1024).decode("utf-8")
+    
+    path = data.split(" ")
+
+
+    if path[1] == "/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        # conn.sendall(b"Hello, World!")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        # conn.sendall(b"404 Not Found")
+    print(data)
+    print(path)
+    
+    # print("Connection from", addr) # Connection from ('127.0.0.1', 34826)
+    
+    # print(conn) # <socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 4221), raddr=('127.0.0.1', 34834)>
+    
+    #  print("Received", conn.recv(1024)) # Received b'GET / HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n'
+    
+    #2 server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")  # wait for client
+    
 if __name__ == "__main__":
     main()
