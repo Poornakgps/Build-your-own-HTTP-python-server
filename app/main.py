e3d60383865b5d5ee5eda15ddaebf42a4d708a66
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2024-05-22 08:45:57 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,49 +1,61 @@
 # Uncomment this to pass the first stage
 import socket
+import re
+class Request(object):
+    def __init__(self, data) -> None:
+        data_str = data.decode()
+        lines = data_str.split("\r\n")
+        method, path, version = lines[0].split()
+        self.method = method
+        self.path = path
+        self.version = version
+        self.user_agent = re_extract(lines[2], r"User-Agent: (.*)")
+class Response(object):
+    def __init__(self, status_code, data) -> None:
+        self.status_code = status_code
+        self.data = data
+        self.status_dict = {200: "OK", 404: "Not Found"}
+    def encode(self) -> bytes:
+        resp_str = "HTTP/1.1 {status_code} {status}\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{body}".format(
+            status_code=self.status_code,
+            status=self.status_dict[self.status_code],
+            len=len(self.data),
+            body=self.data,
+        )
+        return resp_str.encode()
+def re_extract(s, pattern):
+    search = re.search(pattern, s)
+    if search:
+        return search.group(1)
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    
-    conn, addr = server_socket.accept()  # wait for client
-    
-    data = conn.recv(1024).decode("utf-8")
-    
-    path = data.split(" ")
-
-
-    # if path[1] == "/":
-        
-    #     resp = "HTTP/1.1 200 OK\r\n\r\n"
-    #     conn.send(resp.encode())
-    #     # conn.sendall(b"Hello, World!")
-        
-    # elif path[1].startswith("/echo"):
-    #     random_path = path[1][6:]
-    #     response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_path)}\r\n\r\n{random_path}\r\n"
-    #     conn.send(response.encode())
-    #     print(random_path)
-    # else:
-    #     conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        # conn.sendall(b"404 Not Found")
-    # print(data)
-    print(path)
-    user_agent = path[-1]
-    length = 0
-    user_agent = user_agent.split("\r\n")
-    length = len(user_agent[0])
-
-    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{user_agent[0]}\r\n"
-    conn.send(response.encode())
-    # print("Connection from", addr) # Connection from ('127.0.0.1', 34826)
-    
-    # print(conn) # <socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 4221), raddr=('127.0.0.1', 34834)>
-    
-    #  print("Received", conn.recv(1024)) # Received b'GET / HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n'
-    
-    #2 server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")  # wait for client
-    
+    while True:
+        try:
+            conn, addr = server_socket.accept()  # wait for client
+            with conn:
+                print("Connected by", addr)
+                data = conn.recv(1024)
+                print("data: %s" % data)
+                req = Request(data)
+                try:
+                    if req.path == "/":
+                        resp = "HTTP/1.1 200 OK\r\n\r\n"
+                    elif "echo" in req.path:
+                        arg = re_extract(req.path, r"/echo/(.*)")
+                        # print("arg=%s" % arg)
+                        resp = Response(200, arg)
+                    elif req.path == "/user-agent":
+                        resp = Response(200, req.user_agent)
+                    else:
+                        raise Exception("not found")
+                except Exception:
+                    resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+                conn.send(resp.encode())
+        except Exception as e:
+            print("[error] <{}>{}".format(e.__class__.__name__, e))
 if __name__ == "__main__":
     main()
