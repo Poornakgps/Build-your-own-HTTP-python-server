8dd71da458d64a916a498ddff4ad5f45a35ddff8
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2024-05-22 09:01:28 +0530

    get a file

---
+++ app/before_get_a_file.py
@@ -0,0 +1,78 @@
+# Uncomment this to pass the first stage
+import socket
+import re
+from sys import argv
+
+class Request(object):
+    def __init__(self, data) -> None:
+        data_str = data.decode()
+        lines = data_str.split("\r\n")
+        method, path, version = lines[0].split()
+        self.method = method
+        self.path = path
+        self.version = version
+        self.user_agent = re_extract(lines[2], r"User-Agent: (.*)")
+class Response(object):
+    def __init__(self, status_code, data) -> None:
+        self.status_code = status_code
+        self.data = data
+        self.status_dict = {200: "OK", 404: "Not Found"}
+    def encode(self) -> bytes:
+        resp_str = "HTTP/1.1 {status_code} {status}\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{body}".format(
+            status_code=self.status_code,
+            status=self.status_dict[self.status_code],
+            len=len(self.data),
+            body=self.data,
+        )
+        return resp_str.encode()
+def re_extract(s, pattern):
+    search = re.search(pattern, s)
+    if search:
+        return search.group(1)
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    # Uncomment this to pass the first stage
+    #
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    while True:
+        try:
+            conn, addr = server_socket.accept()  # wait for client
+            with conn:
+                print("Connected by", addr)
+                data = conn.recv(1024)
+                print("data: %s" % data)
+                req = Request(data)
+                try:
+                    if req.path == "/":
+                        resp = "HTTP/1.1 200 OK\r\n\r\n"
+                    elif "echo" in req.path:
+                        arg = re_extract(req.path, r"/echo/(.*)")
+                        # print("arg=%s" % arg)
+                        resp = Response(200, arg)
+
+                    elif req.path == "/user-agent":
+                        resp = Response(200, req.user_agent)
+                    elif "/files" in req.path:
+                        f_name = req.path.split("/")[-1]
+                        try:
+                            with open(argv[2] + f_name) as f:
+                                content = f.read()
+                                cont_length = len(content)
+                                return (
+                                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "
+                                    + str(cont_length)
+                                    + "\r\n\r\n"
+                                    + content
+                                )
+                        except FileNotFoundError:
+                            return "HTTP/1.1 404 Not Found\r\n\r\n"
+                    else:
+                        raise Exception("not found")
+                except Exception:
+                    resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+                conn.send(resp.encode())
+        except Exception as e:
+            print("[error] <{}>{}".format(e.__class__.__name__, e))
+if __name__ == "__main__":
+    main()
--- app/main.py
+++ app/main.py
@@ -1,78 +1,58 @@
-# Uncomment this to pass the first stage
 import socket
-import re
+import threading
 from sys import argv
-
-class Request(object):
-    def __init__(self, data) -> None:
-        data_str = data.decode()
-        lines = data_str.split("\r\n")
-        method, path, version = lines[0].split()
-        self.method = method
-        self.path = path
-        self.version = version
-        self.user_agent = re_extract(lines[2], r"User-Agent: (.*)")
-class Response(object):
-    def __init__(self, status_code, data) -> None:
-        self.status_code = status_code
-        self.data = data
-        self.status_dict = {200: "OK", 404: "Not Found"}
-    def encode(self) -> bytes:
-        resp_str = "HTTP/1.1 {status_code} {status}\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{body}".format(
-            status_code=self.status_code,
-            status=self.status_dict[self.status_code],
-            len=len(self.data),
-            body=self.data,
-        )
-        return resp_str.encode()
-def re_extract(s, pattern):
-    search = re.search(pattern, s)
-    if search:
-        return search.group(1)
+def getResponseTxt(method, path, response_body):
+    if path == "/":
+        return "HTTP/1.1 200 OK\r\n\r\n"
+    elif "/echo" in path:
+        content = path.split("/")[-1]
+        content_length = len(path.split("/")[-1])
+        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{content}"
+    elif "/user-agent" in path:
+        content_length = len(response_body)
+        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{response_body}"
+    elif "/files" in path:
+        f_name = path.split("/")[-1]
+        try:
+            with open(argv[2] + f_name) as f:
+                content = f.read()
+                cont_length = len(content)
+                return (
+                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "
+                    + str(cont_length)
+                    + "\r\n\r\n"
+                    + content
+                )
+        except FileNotFoundError:
+            return "HTTP/1.1 404 Not Found\r\n\r\n"
+    else:
+        return "HTTP/1.1 404 Not Found\r\n\r\n"
+def parseRequest(client):
+    lines = client.recv(4096).decode().split(" ")
+    # print(lines[-1].strip())
+    method = lines[0]
+    path = lines[1]
+    response_body = lines[-1].strip()
+    return method, path, response_body
+def sendResponse(client, text):
+    client.sendall(text.encode("UTF-8"))
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     # Uncomment this to pass the first stage
     #
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket.accept() # wait for client
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen()
+    # server_socket.accept()
     while True:
-        try:
-            conn, addr = server_socket.accept()  # wait for client
-            with conn:
-                print("Connected by", addr)
-                data = conn.recv(1024)
-                print("data: %s" % data)
-                req = Request(data)
-                try:
-                    if req.path == "/":
-                        resp = "HTTP/1.1 200 OK\r\n\r\n"
-                    elif "echo" in req.path:
-                        arg = re_extract(req.path, r"/echo/(.*)")
-                        # print("arg=%s" % arg)
-                        resp = Response(200, arg)
-
-                    elif req.path == "/user-agent":
-                        resp = Response(200, req.user_agent)
-                    elif "/files" in req.path:
-                        f_name = req.path.split("/")[-1]
-                        try:
-                            with open(argv[2] + f_name) as f:
-                                content = f.read()
-                                cont_length = len(content)
-                                return (
-                                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "
-                                    + str(cont_length)
-                                    + "\r\n\r\n"
-                                    + content
-                                )
-                        except FileNotFoundError:
-                            return "HTTP/1.1 404 Not Found\r\n\r\n"
-                    else:
-                        raise Exception("not found")
-                except Exception:
-                    resp = "HTTP/1.1 404 Not Found\r\n\r\n"
-                conn.send(resp.encode())
-        except Exception as e:
-            print("[error] <{}>{}".format(e.__class__.__name__, e))
+        client, addr = server_socket.accept()
+        method, path, response_body = parseRequest(client)
+        print("remote", method, path, response_body)
+        response = getResponseTxt(method, path, response_body)
+        # print('response',response)
+        threading.Thread(target=sendResponse, args=(client, response)).start()
+    # client.close()
 if __name__ == "__main__":
     main()
