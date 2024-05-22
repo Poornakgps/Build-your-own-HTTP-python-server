090a3cd96961cbb2d8a6034b00520d7b7229429d
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2024-05-22 09:21:30 +0530

    final

--- app/main.py
+++ app/main.py
@@ -1,100 +1,77 @@
 import socket
 import threading
+import argparse
 import os
-import sys
-STATUS_200 = "HTTP/1.1 200 OK\r\n"
-STATUS_201 = "HTTP/1.1 201 Created\r\n\r\n"
-STATUS_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
-# Function that defines the behaviour for handling a single client connection
-def handle_connection(connection_socket, address):
-    # Extract URL path - request target
-    request_data = connection_socket.recv(4096).decode()
-    print(f"Received chunk:\n{request_data}")
-    # Parse the HTTP request
-    lines = request_data.split("\r\n")
-    print(lines)
-    headers = {}
-    for header in lines[1:]:
-        if header == "":
-            break
-        key, value = header.split(": ")
-        print(key, value)
-        headers[key] = value
-    start_line = lines[0]
-    method, request_target, http_version = start_line.split(" ")
-    print(request_data)
-    request_body = lines[-1]
-    if request_target == "/":
-        connection_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    elif request_target.startswith("/user-agent"):
-        response = (
-            f"{STATUS_200}"
-            "Content-Type: text/plain\r\n"
-            f"Content-Length: {len(headers.get('User-Agent'))}\r\n"
-            "\r\n"
-            f"{headers.get('User-Agent')}"
-        )
-        # HTTP response
-        connection_socket.sendall(response.encode())
-    elif request_target.startswith("/echo"):
-        endpoint = (request_target.split("/"))[-1]
-        response = (
-            f"{STATUS_200}"
-            "Content-Type: text/plain\r\n"
-            f"Content-Length: {len(endpoint)}\r\n"
-            "\r\n"
-            f"{endpoint}"
-        )
-        # HTTP response
-        connection_socket.sendall(response.encode())
-    elif request_target.startswith("/files") and method == "GET":
-        filename = (request_target.split("/"))[2]
-        print(filename)
-        directory = sys.argv[2]
-        path = "".join([directory, filename])
-        if os.path.exists(path):
-            with open(path, "r") as file:
-                content = file.read()
-                response = (
-                    f"{STATUS_200}"
-                    "Content-Type: application/octet-stream\r\n"
-                    f"Content-Length: {len(content)}\r\n"
-                    "\r\n"
-                    f"{content}"
-                )
-                # HTTP response
-                connection_socket.sendall(response.encode())
-        else:
-            connection_socket.sendall(STATUS_404.encode())
-    elif request_target.startswith("/files") and method == "POST":
-        filename = (request_target.split("/"))[2]
-        print(filename)
-        directory = sys.argv[2]
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        path = "".join([directory, filename])
-        with open(path, "w") as file:
-            file.write(request_body)
-        response = f"{STATUS_201}"
-        # HTTP response
-        connection_socket.sendall(response.encode())
-    else:
-        # HTTP response
-        connection_socket.sendall(STATUS_404.encode())
-    connection_socket.close()
-# Main function that accepts incoming connections and create threadss
+import gzip
+parser = argparse.ArgumentParser(description="takes directory")
+parser.add_argument("--directory", dest="directory")
+args = parser.parse_args()
+directory = args.directory
+class Request:
+    def __init__(self, request: bytes):
+        request_string = request.decode()
+        body_split = request_string.split("\r\n\r\n")
+        lines = body_split[0].split("\r\n")
+        status = lines[0].split()
+        self.method = status[0]
+        self.path = status[1] 
+        self.version = status[2]
+        self.headers = {}
+        for x in range(1, len(lines)):
+            key, value = lines[x].split(": ")
+            self.headers[key.lower()] = value
+        self.body = body_split[1]
+        
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    # Create TCP server socket
-    server_socket = socket.create_server(("localhost", 4221))
-    # Accept different TCP connections and handle them in separate threads
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
-        (connection_socket, address) = server_socket.accept()  # wait for client
-        print(f"Received connection from {address}")
-        connection_thread = threading.Thread(
-            target=handle_connection, args=(connection_socket, address)
-        )
-        connection_thread.start()
+        client_socket, client_address = server_socket.accept() # wait for client
+        threading.Thread(target=request_handler, args=(client_socket, client_address,), daemon=True).start()
+def request_handler(client_socket, client_address):
+    data = client_socket.recv(1024)
+    request = Request(data)
+    if request.path == "/":
+        client_socket.sendall(build_response("200 OK", "").encode())
+    path_resources = request.path.split("/")
+    print(path_resources[1])
+    print(len(path_resources))
+    print(request.method)
+    if path_resources[1] == "echo" and len(path_resources) == 3 and request.method == "GET":
+        print(request.headers)
+        print(request.headers.get("accept-encoding"))
+        if request.headers.get("accept-encoding") is None:
+            client_socket.sendall(build_response("200 OK", f"Content-Type: text/plain\r\nContent-Length: {len(path_resources[2])}", path_resources[2]).encode())
+        if "gzip" in request.headers.get("accept-encoding"):
+            # client_socket.sendall(build_response("200 OK", f"Content-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(path_resources[2])}", path_resources[2]).encode())
+            response_body = gzip.compress(path_resources[2].encode())
+            client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n".encode() + response_body)
+    if path_resources[1] == "echo" and request.method == "GET":
+        client_socket.sendall(build_response("200 OK", f"Content-Type: text/plain\r\nContent-Length: {len(path_resources[2])}", path_resources[2]).encode())
+    if path_resources[1] == "user-agent" and request.method == "GET":
+        client_socket.sendall(build_response("200 OK", f"Content-Type: text/plain\r\nContent-Length: {len(request.headers["user-agent"])}", request.headers["user-agent"]).encode())
+    if path_resources[1] == "files" and request.method == "GET":
+        try:
+            file_path = os.path.join(directory, path_resources[2])
+            with open(file_path, "r") as f:
+                file_content = f.read()
+                client_socket.sendall(build_response("200 OK", f"Content-Type: application/octet-stream\r\nContent-Length: {len(file_content)}", file_content).encode())
+        except Exception:
+            client_socket.sendall(build_response("404 Not Found", "Content-Length: 0").encode())
+    if path_resources[1] == "files" and request.method == "POST":
+        try:
+            file_path = os.path.join(directory, path_resources[2])
+            print("File path: ", file_path)
+            print("File content: ", request.body)
+            with open(file_path, "w") as f:
+                f.write(request.body)
+                client_socket.sendall(build_response("201 Created", "Content-Length: 0").encode())
+        except Exception as e:
+            print("Error: ", e)
+            client_socket.sendall(build_response("500 Internal Error", "Content-Length: 0").encode())
+            
+    else:
+        client_socket.sendall(build_response("404 Not Found", "").encode())
+def build_response(status: str, headers: str, body: str = "") -> str:
+    return f"HTTP/1.1 {status}\r\n{headers}\r\n\r\n{body}"
 if __name__ == "__main__":
     main()
